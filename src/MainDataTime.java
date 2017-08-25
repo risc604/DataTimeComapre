419f4cd35c34845ad6d0d07afaaa68b3ed1c90f2
tomcat <tomcat.cheng@microlife.com.tw>
2017-08-25 14:44:25 +0800

    init java date test function.

---
+++ src/MainDataTime.java
@@ -0,0 +1,45 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainDataTime 
+{
+	public static Date stringToDateFormat(String srcDate) throws ParseException 
+	{
+		String pattern1 = "yyMMddHHmm";
+		SimpleDateFormat sdf1 = new SimpleDateFormat(pattern1);
+	
+		Date date = sdf1.parse(srcDate);
+		
+		//debug
+		System.out.println("stringToDateFormat(), date: " + date);
+		
+		return date;
+	}
+	
+	
+	public static void DataTimeTest() throws ParseException 
+	{
+		String srcDate = "1708181648";
+		Date recordDT = stringToDateFormat(srcDate);
+		Date currentDT = new Date(System.currentTimeMillis());
+		
+		System.out.println(	"Data DT: " + recordDT.toString() + 
+							", currentDT: " + currentDT.toString() + 
+							", compare DT: " + recordDT.compareTo(currentDT));
+		
+		System.out.println("currentDT - recordDT = " + 
+							new Date(currentDT.getTime() - recordDT.getTime()) );
+	}
+		
+	public static void main(String[] args)
+	{
+		try {
+			DataTimeTest();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
